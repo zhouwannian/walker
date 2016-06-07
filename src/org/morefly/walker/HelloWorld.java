eba5ba7d0f4c43af728ddc6289bec5daca840c3a
zhouwannian <447653460@qq.com>
2016-06-07 16:26:23 +0800

    提交到本地仓库

---
+++ src/org/morefly/walker/HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+ * copyright@org.morefly
+ * @contact www.morefly.com.cn
+ */
+package org.morefly.walker;
+/**
+ * 
+ * @author zhouwn
+ * @since 2016年6月7日下午4:24:25
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+}
