8a9218478fc4a6431efe193ba385e5feb756e643
zhouwannian <447653460@qq.com>
2016-06-07 16:27:15 +0800

    新增本地方法

--- src/org/morefly/walker/HelloWorld.java
+++ src/org/morefly/walker/HelloWorld.java
@@ -12,4 +12,11 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
+	
+	/**
+	 * 方法1
+	 */
+	private void fun1(){
+		System.out.println(123);
+	}
 }
